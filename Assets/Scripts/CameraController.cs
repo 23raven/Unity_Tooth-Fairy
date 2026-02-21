94528e5210a8be955b7332a5e862af9fd326bdea
23raven <23raven.games@gmail.com>
2026-02-21 14:34:36 +0500

    add CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player; // сюда перетаскиваешь Player в Inspector
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        if (player == null) return;
+
+        Vector3 targetPos = new Vector3(
+            player.position.x,
+            player.position.y,
+            transform.position.z // ⭐ чтобы камера не меняла глубину
+        );
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            targetPos,
+            smoothSpeed * Time.deltaTime
+        );
+    }
+}
