17465a2c7b659744e4c719bdc1815fcb08f81036
23raven <23raven.games@gmail.com>
2026-03-17 23:16:31 +0500

    set up pause menu

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class Pause : MonoBehaviour
 {
     private bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
 
     void Update()
     {
@@ -18,10 +25,12 @@ public class Pause : MonoBehaviour
 
         if (isPaused)
         {
+            pauseMenuUI.SetActive(true);
             Time.timeScale = 0f; // Остановить время
         }
         else
         {
+            pauseMenuUI.SetActive(false);
             Time.timeScale = 1f; // Вернуть время
         }
     }
